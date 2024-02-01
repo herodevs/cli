34b2514c6be390e13dc91cb9e882fec1bb8352ae
Mike Brocchi <brocco@users.noreply.github.com>
2024-02-01 09:49:45 -0500

    Migrate version checking logic to use a more stable library

--- src/shared/lib/version-update.ts
+++ src/shared/lib/version-update.ts
@@ -1,47 +1,64 @@
 import { run } from './shell';
 import { env } from '../config';
 import { Command } from '@oclif/core';
-import latestVersion from '@badisi/latest-version';
+import * as getJson from 'get-json';
 import { Color, color } from '../enums';
 
 const red = color(Color.FgRed);
 const yellow = color(Color.FgYellow);
 
 function reconstituteCommandPositionalsAndFlags(command: Command | any): string {
-    return [
-        ...command.id?.split(':'),
-        ...Object.keys(command.flags).map((flag) => `--${flag}='${command.flags[flag]}'`)
-    ].join(' ');
+  return [
+    ...command.id?.split(':'),
+    ...Object.keys(command.flags).map((flag) => `--${flag}='${command.flags[flag]}'`),
+  ].join(' ');
 }
 
-export async function isVersionUpToDate(command: Command, quietIfSuccessful = false): Promise<boolean> {
-    const versionData = await latestVersion(env.packageName);
-    if (versionData && versionData.latest && versionData.latest === env.packageVersion) {
-        if (!quietIfSuccessful) {
-            command.log(`${env.packageName}@${versionData.latest} is up to date`);
-        }
-        return true;
+async function getLatestVersion(pkgName: string) {
+  return getJson(`https://registry.npmjs.org/${pkgName}`).then((packageData) => {
+    return packageData['dist-tags'].latest;
+  });
+}
+
+export async function isVersionUpToDate(
+  command: Command,
+  quietIfSuccessful = false
+): Promise<boolean> {
+  const latestVersion = await getLatestVersion(env.packageName);
+  if (latestVersion === env.packageVersion) {
+    if (!quietIfSuccessful) {
+      command.log(`${env.packageName}@${latestVersion} is up to date`);
     }
-    command.log(`${yellow('Your version:', red(`${env.packageName}@${env.packageVersion}`), `is not up to date`)}`);
-    command.log(`${yellow('Latest version:', red(`${env.packageName}@${versionData.latest}`))}`);
-    return false;
+    return true;
+  }
+  command.log(
+    `${yellow(
+      'Your version:',
+      red(`${env.packageName}@${env.packageVersion}`),
+      `is not up to date`
+    )}`
+  );
+  command.log(`${yellow('Latest version:', red(`${env.packageName}@${latestVersion}`))}`);
+  return false;
 }
 
 export async function ensureVersionIsUpToDate(command: Command): Promise<void> {
-    const versionUpToDate = await isVersionUpToDate(command);
-    if (!versionUpToDate) {
-        // see `update-plugin-behavior` branch for previous implementation of auto-update
+  const versionUpToDate = await isVersionUpToDate(command);
+  if (!versionUpToDate) {
+    // see `update-plugin-behavior` branch for previous implementation of auto-update
 
-        const commandAndPositionals = reconstituteCommandPositionalsAndFlags(command);
-        
-        command.log([
-            `\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\n`,
-            `${red(`ERROR:`)} Did not run command.\n\n`,
-            ` - Rerun your command with the ${yellow('@latest')} tag to ensure correct output:\n\n\n`,
-            `\t${yellow(`npx ${env.packageName}@latest ${commandAndPositionals}`)}\n`,
-            `\n\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n`,
-        ].join(' '));
+    const commandAndPositionals = reconstituteCommandPositionalsAndFlags(command);
 
-        return process.exit(1);
-    }
+    command.log(
+      [
+        `\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\n`,
+        `${red(`ERROR:`)} Did not run command.\n\n`,
+        ` - Rerun your command with the ${yellow('@latest')} tag to ensure correct output:\n\n\n`,
+        `\t${yellow(`npx ${env.packageName}@latest ${commandAndPositionals}`)}\n`,
+        `\n\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n`,
+      ].join(' ')
+    );
+
+    return process.exit(1);
+  }
 }
