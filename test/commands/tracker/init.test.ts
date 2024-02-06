23cd03f8a53e57ddff5eeb298493ead3543972c7
Mike Brocchi <brocco@users.noreply.github.com>
2024-02-06 11:03:06 -0500

    Clean up tests and linting

--- test/commands/tracker/init.test.ts
+++ test/commands/tracker/init.test.ts
@@ -1,17 +1,16 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test';
 
 describe('tracker:init', () => {
-  test
-  .stdout()
-  .command(['tracker:init'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['tracker:init', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
-})
+  // test
+  // .stdout()
+  // .command(['tracker:init'])
+  // .it('runs hello', ctx => {
+  //   expect(ctx.stdout).to.contain('hello world')
+  // })
+  // test
+  // .stdout()
+  // .command(['tracker:init', '--name', 'jeff'])
+  // .it('runs hello --name jeff', ctx => {
+  //   expect(ctx.stdout).to.contain('hello jeff')
+  // })
+});
--- test/commands/tracker/run.test.ts
+++ test/commands/tracker/run.test.ts
@@ -1,17 +1,16 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test';
 
 describe('tracker:run', () => {
-  test
-  .stdout()
-  .command(['tracker:run'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['tracker:run', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
-})
+  // test
+  // .stdout()
+  // .command(['tracker:run'])
+  // .it('runs hello', ctx => {
+  //   expect(ctx.stdout).to.contain('hello world')
+  // })
+  // test
+  // .stdout()
+  // .command(['tracker:run', '--name', 'jeff'])
+  // .it('runs hello --name jeff', ctx => {
+  //   expect(ctx.stdout).to.contain('hello jeff')
+  // })
+});
