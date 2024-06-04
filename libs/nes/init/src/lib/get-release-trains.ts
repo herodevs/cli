5000ce32f33b4ac6b368d175f4ee859a972c81fa
Mike Brocchi <brocco@users.noreply.github.com>
2024-06-04 09:17:37 -0400

    remove graphql deps

--- libs/nes/init/src/lib/get-release-trains.ts
+++ libs/nes/init/src/lib/get-release-trains.ts
@@ -1,63 +1,63 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client/core';
+// import { ApolloClient, gql, InMemoryCache } from '@apollo/client/core';
 import { ProjectType } from '@herodevs/core-types';
-// import { mockTrains } from './mock-trains';
+import { mockTrains } from './mock-trains';
 import { ReleaseTrain } from './models';
 
 export async function getReleaseTrains(
   accessToken: string,
   types: ProjectType[]
 ): Promise<ReleaseTrain[]> {
-  // return new Promise((resolve) => {
-  //   setTimeout(() => {
-  //     resolve(mockTrains as unknown as ReleaseTrain[]);
-  //   }, 250);
-  // });
-
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: 'https://api.nes.herodevs.com/graphql',
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mockTrains as unknown as ReleaseTrain[]);
+    }, 250);
   });
 
-  try {
-    const queryResult = await client.query({
-      query: gql`
-        query RT($input: LicensingReleaseTrainsInput) {
-          licensing {
-            releaseTrains(input: $input) {
-              results {
-                key
-                name
-                products {
-                  id
-                  key
-                  name
-                }
-                entries {
-                  packageVersion {
-                    id
-                    name
-                    fqns
-                    origination {
-                      name
-                      type
-                      version
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      `,
-      variables: { input: { tenantId: 1000, byToken: accessToken } },
-    });
+  // const client = new ApolloClient({
+  //   cache: new InMemoryCache(),
+  //   uri: 'https://api.nes.herodevs.com/graphql',
+  // });
 
-    const results = queryResult.data?.licensing?.releaseTrains?.results;
-    if (!results) {
-      throw new Error(`Error getting release trains`);
-    }
-    return results;
-  } catch (error) {
-    throw new Error(`Error getting release trains`);
-  }
+  // try {
+  //   const queryResult = await client.query({
+  //     query: gql`
+  //       query RT($input: LicensingReleaseTrainsInput) {
+  //         licensing {
+  //           releaseTrains(input: $input) {
+  //             results {
+  //               key
+  //               name
+  //               products {
+  //                 id
+  //                 key
+  //                 name
+  //               }
+  //               entries {
+  //                 packageVersion {
+  //                   id
+  //                   name
+  //                   fqns
+  //                   origination {
+  //                     name
+  //                     type
+  //                     version
+  //                   }
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     `,
+  //     variables: { input: { tenantId: 1000, byToken: accessToken } },
+  //   });
+
+  //   const results = queryResult.data?.licensing?.releaseTrains?.results;
+  //   if (!results) {
+  //     throw new Error(`Error getting release trains`);
+  //   }
+  //   return results;
+  // } catch (error) {
+  //   throw new Error(`Error getting release trains`);
+  // }
 }
