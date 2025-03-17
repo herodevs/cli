1c3568b0a3278dfe51d68c48bc580c4593b6e6b5
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-03-17 12:29:30 -0500

    chore: fix prerun hook filename

--- test/hooks/prerun/command-context-hook.test.ts
+++ test/hooks/prerun/command-context-hook.test.ts
@@ -2,9 +2,9 @@ import { ok } from 'node:assert';
 import { describe, it } from 'node:test';
 import { runHook } from '@oclif/test';
 
-describe('hooks', () => {
+describe('hooks: prerun', () => {
   it('shows a message', async () => {
-    const { result } = await runHook('prerun', { id: 'report committers' });
+    const { result } = await runHook('prerun', { id: 'report purls' });
     ok((result as unknown as { successes: unknown[] }).successes);
   });
 });
