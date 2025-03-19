d7c9c520b317c7e0dae94023eeaee979464e09e6
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-03-19 16:14:43 -0500

    feat: install plugin-update and create updater

---
+++ src/config/update.ts
@@ -0,0 +1,15 @@
+export const updateConfig = {
+  // Enable auto-updates
+  autoupdate: true,
+
+  // Check for updates every 24 hours
+  autoupdateInterval: 24 * 60 * 60 * 1000,
+
+  // Version update notifications
+  // During beta, we want to show all updates
+  showMajorVersionUpdate: true,
+  showMinorVersionUpdate: true,
+  showPatchVersionUpdate: true,
+};
+
+export default updateConfig;
