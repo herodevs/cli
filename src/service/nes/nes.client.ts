4f3c9b520ce55883f33adf55a435e50742f88266
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-03-19 13:20:02 -0500

    feat: refactor nes client

--- src/service/nes/nes.client.ts
+++ src/service/nes/nes.client.ts
@@ -1,7 +1,8 @@
 // TODO move this to another lib altogether!
 
-import * as apollo from '@apollo/client/core/index.js';
-import { type ScanResult, SbomScanner as sbomScanner } from '../nes/modules/sbom.ts';
+import { ApolloClient } from '../../api/client.ts';
+import type { ScanResult } from '../../api/types/nes.ts';
+import { SbomScanner } from './nes.svc.ts';
 
 export interface NesClient {
   scan: {
@@ -9,62 +10,21 @@ export interface NesClient {
   };
 }
 
-export interface ApolloHelper {
-  mutate<T, V extends apollo.OperationVariables>(
-    mutation: apollo.DocumentNode,
-    variables?: V,
-  ): Promise<apollo.FetchResult<T>>;
-  query<T, V extends apollo.OperationVariables | undefined = undefined>(
-    query: apollo.DocumentNode,
-    variables?: V,
-  ): Promise<apollo.ApolloQueryResult<T>>;
-}
-
-export const createApollo = (url: string) =>
-  new apollo.ApolloClient({
-    cache: new apollo.InMemoryCache({
-      addTypename: false,
-    }),
-    headers: {
-      'User-Agent': `hdcli/${process.env.npm_package_version ?? 'unknown'}`,
-    },
-    link: apollo.ApolloLink.from([
-      new apollo.HttpLink({
-        uri: url,
-      }),
-    ]),
-  });
-
-export class NesApolloError extends Error {}
-export class NesApolloClient implements ApolloHelper, NesClient {
+export class NesApolloClient implements NesClient {
   scan = {
-    sbom: sbomScanner(this),
+    sbom: SbomScanner(this),
   };
-  #apollo: apollo.ApolloClient<apollo.NormalizedCacheObject>;
+  #apollo: ApolloClient;
 
   constructor(url: string) {
-    this.#apollo = createApollo(url);
+    this.#apollo = new ApolloClient(url);
   }
 
-  mutate<T, V extends apollo.OperationVariables>(mutation: apollo.DocumentNode, variables?: V) {
-    return this.#apollo
-      .mutate<T, V>({
-        mutation,
-        variables,
-      })
-      .catch((error) => {
-        throw new NesApolloError('Failed GQL Mutation', error);
-      });
+  mutate<T, V extends Record<string, unknown>>(mutation: any, variables?: V) {
+    return this.#apollo.mutate<T, V>(mutation, variables);
   }
 
-  query<T, V extends apollo.OperationVariables | undefined>(query: apollo.DocumentNode, variables?: V) {
-    return this.#apollo
-      .query<T>({
-        query,
-        variables,
-      })
-      .catch((error) => {
-        throw new NesApolloError('Failed GQL Query', error);
-      });
+  query<T, V extends Record<string, unknown> | undefined>(query: any, variables?: V) {
+    return this.#apollo.query<T, V>(query, variables);
   }
 }
