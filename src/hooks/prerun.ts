da7be2e2eacabb728d57f485db7cc21ed45027c0
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-03-24 16:14:24 -0500

    chore: cleanup prerun hook
    Since UX actions are going to stderr anyways and not
    interfering with stdout redirection, we can ignore.

--- src/hooks/prerun.ts
+++ src/hooks/prerun.ts
@@ -1,14 +1,10 @@
 import type { Hook } from '@oclif/core';
-import { ux } from '@oclif/core';
 import debug from 'debug';
 
 const hook: Hook<'prerun'> = async (opts) => {
-  // If JSON flag is enabled, silence ux actions and debug
-  if (opts.argv.includes('--json')) {
-    const noop = () => {};
+  // If JSON flag is enabled, silence debug logging
+  if (opts.Command.prototype.jsonEnabled()) {
     debug.disable();
-    ux.action.start = noop;
-    ux.action.stop = noop;
   }
 };
 
