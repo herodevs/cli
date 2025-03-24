085bd587e4aab57bb1080bc6e4e6cba0b89db41f
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-03-24 10:27:06 -0500

    chore: add prerun hook

--- src/hooks/prerun/command-context-hook.ts
+++ src/hooks/prerun/command-context-hook.ts
@@ -2,7 +2,8 @@ import type { Hook } from '@oclif/core';
 
 import { initOclifLog, log } from '../../service/log.svc.ts';
 
-const hook: Hook<'prerun'> = async (opts) => {
+const hook: Hook.Prerun = async (opts) => {
+  console.log('prerun hook');
   initOclifLog(opts.context.log, opts.context.log, opts.context.debug);
   log.info = opts.context.log || log.info;
   log.warn = opts.context.log || log.warn;
