14761f0ea9285a496c5b91610e9c6465f2d817cc
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-03-24 11:07:33 -0500

    chore: clean up logging service
    Use built-in debug features and the getLogger utility. Get rid of prerun
    hook.

--- src/hooks/init/update.ts
+++
@@ -1,7 +0,0 @@
-import type { Hook } from '@oclif/core';
-import updateConfig from '../../config/update.js';
-
-export const initUpdate: Hook<'init'> = async ({ config }) => {
-  // Apply update configuration
-  Object.assign(config, updateConfig);
-};
--- src/service/log.svc.ts
+++ src/service/log.svc.ts
@@ -1,25 +1,3 @@
-/**
- * A simple logging construct when you
- * don't have the command instance handy
- */
-export const log = {
-  info: (_message?: unknown, ...args: unknown[]) => {
-    console.log('[default_log]', ...args);
-  },
-  warn: (_message?: unknown, ...args: unknown[]) => {
-    console.warn('[default_warn]', ...args);
-  },
-  debug: (_message?: unknown, ...args: unknown[]) => {
-    console.debug('[default_debug]', ...args);
-  },
-};
+import { getLogger } from '@oclif/core';
 
-export const initOclifLog = (
-  info: (message?: unknown, ...args: unknown[]) => void,
-  warn: (message?: unknown, ...args: unknown[]) => void,
-  debug: (message?: unknown, ...args: unknown[]) => void,
-) => {
-  log.info = info;
-  log.warn = warn;
-  log.debug = debug;
-};
+export const log = getLogger('hd');
