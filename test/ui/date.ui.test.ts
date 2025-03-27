a6308cc5f9fbd8c1753e05f5529f7e56e4ecce28
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-03-27 11:31:10 -0500

    chore: create date.ui.test

---
+++ test/ui/date.ui.test.ts
@@ -0,0 +1,4 @@
+import assert from 'node:assert';
+import { afterEach, beforeEach, describe, it } from 'node:test';
+import { parseDateToString } from '../../src/ui/date.ui.ts';
+describe('EOL UI', () => {});
