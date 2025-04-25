7c5d7a256b0f816100d8ea26fe790a8f6cb70bdb
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-04-25 14:24:24 -0500

    feat: run `scan eol -t` by default (#205)
    When the user provides no arguments to the cli, by default it
    should run `scan eol-t`.

--- bin/dev.js
+++ bin/dev.js
@@ -11,4 +11,11 @@ process.env.GRAPHQL_HOST = 'https://api.dev.nes.herodevs.com';
 // Prod
 // process.env.GRAPHQL_HOST = 'https://api.nes.herodevs.com';
 
+// If no command is provided, default to scan:eol -t
+// See https://github.com/oclif/oclif/issues/277#issuecomment-657352674 for more info
+if (process.argv.length === 2) {
+  process.argv[2] = 'scan:eol';
+  process.argv[3] = '-t';
+}
+
 await execute({ development: true, dir: import.meta.url });
--- bin/run.js
+++ bin/run.js
@@ -2,4 +2,11 @@
 
 import { execute } from '@oclif/core';
 
+// If no command is provided, default to scan:eol -t
+// See https://github.com/oclif/oclif/issues/277#issuecomment-657352674 for more info
+if (process.argv.length === 2) {
+  process.argv[2] = 'scan:eol';
+  process.argv[3] = '-t';
+}
+
 await execute({ dir: import.meta.url });
