5e636793cf95853a7f1f800ed446d069f4a62db0
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-04-25 14:07:09 -0500

    chore: update based on pr feedback

--- bin/dev.js
+++ bin/dev.js
@@ -13,7 +13,6 @@ process.env.GRAPHQL_HOST = 'https://api.dev.nes.herodevs.com';
 
 // If no command is provided, default to scan:eol -t
 // See https://github.com/oclif/oclif/issues/277#issuecomment-657352674 for more info
-// There is no canonical way to do this, so we're using a hacky solution
 if (process.argv.length === 2) {
   process.argv[2] = 'scan:eol';
   process.argv[3] = '-t';
--- bin/run.js
+++ bin/run.js
@@ -4,7 +4,6 @@ import { execute } from '@oclif/core';
 
 // If no command is provided, default to scan:eol -t
 // See https://github.com/oclif/oclif/issues/277#issuecomment-657352674 for more info
-// There is no canonical way to do this, so we're using a hacky solution
 if (process.argv.length === 2) {
   process.argv[2] = 'scan:eol';
   process.argv[3] = '-t';
