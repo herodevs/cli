80c5622ca82621a2bb378c056155bbb831636c0a
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-05-13 12:16:50 -0500

    feat: log update-notifier message to stdout (#223)
    * feat: output update-notifier message to stdout
    
    * chore: add more specs for npm update hook
    
    * chore: delete unnecessary comment
    
    * chore: set should notify in npm script to true
    
    Update src/hooks/npm-update-notifier.ts
    
    Co-authored-by: Marco Ippolito <marcoippolito54@gmail.com>
    
    Update src/hooks/npm-update-notifier.ts
    
    Co-authored-by: Marco Ippolito <marcoippolito54@gmail.com>
    
    Update src/hooks/npm-update-notifier.ts
    
    Co-authored-by: Marco Ippolito <marcoippolito54@gmail.com>
    
    * chore: run linting
    
    * chore: remove prev1 from dist tag type
    
    * chore: remove unused import
    
    ---------
    
    Co-authored-by: Marco Ippolito <marcoippolito54@gmail.com>

--- src/hooks/npm-update-notifier.ts
+++ src/hooks/npm-update-notifier.ts
@@ -1,15 +1,32 @@
 import type { Hook } from '@oclif/core';
 import updateNotifier, { type UpdateInfo } from 'update-notifier';
 import pkg from '../../package.json' with { type: 'json' };
+import { debugLogger } from '../service/log.svc.ts';
 
 const updateNotifierHook: Hook.Init = async (options) => {
+  debugLogger('pkg.version', pkg.version);
+
+  const distTag = getDistTag(pkg.version);
+  debugLogger('distTag', distTag);
+  const ONE_DAY_MS = 1000 * 60 * 60 * 24;
+  // If we're on the latest dist-tag, check for updates every time
+  const updateCheckInterval = distTag === 'latest' ? 0 : ONE_DAY_MS;
+
+  debugLogger('updateCheckInterval', updateCheckInterval);
+
   const notifier = updateNotifier({
     pkg,
-    updateCheckInterval: 1000 * 60 * 60 * 24, // Check once per day
+    distTag,
+    updateCheckInterval,
+    shouldNotifyInNpmScript: true,
   });
+
+  debugLogger('updateNotifierHook', { notifier });
 
   if (notifier.update) {
     const notification = handleUpdate(notifier.update, pkg.version);
+    debugLogger('notification', notification);
+
     if (notification) {
       notifier.notify(notification);
     }
@@ -18,11 +35,19 @@ const updateNotifierHook: Hook.Init = async (options) => {
 
 export default updateNotifierHook;
 
+type DistTag = 'latest' | 'beta' | 'alpha' | 'next';
+
+export function getDistTag(version: string): DistTag {
+  if (version.includes('-beta')) return 'beta';
+  if (version.includes('-alpha')) return 'alpha';
+  if (version.includes('-next')) return 'next';
+  return 'latest';
+}
+
 export function handleUpdate(update: UpdateInfo, currentVersion: string) {
-  const isPreV1 = currentVersion.startsWith('0.');
-  const isBeta = currentVersion.includes('-beta') || update.latest.includes('-beta');
-  const isAlpha = currentVersion.includes('-alpha') || update.latest.includes('-alpha');
-  const isNext = currentVersion.includes('-next') || update.latest.includes('-next');
+  const isPreV1 = currentVersion.startsWith('0.') || update.latest.startsWith('0.');
+  const currentDistTag = getDistTag(currentVersion);
+  const updateDistTag = getDistTag(update.latest);
 
   let message = `Update available! v${currentVersion} → v${update.latest}`;
 
@@ -35,7 +60,7 @@ export function handleUpdate(update: UpdateInfo, currentVersion: string) {
    * [1]https://semver.org/#spec-item-4
    * [2]https://antfu.me/posts/epoch-semver#leading-zero-major-versioning
    */
-  if (isPreV1 || isBeta || isAlpha || isNext || update.type === 'major') {
+  if (isPreV1 || currentDistTag !== 'latest' || updateDistTag !== 'latest' || update.type === 'major') {
     message += '\nThis update may contain breaking changes.';
   }
 
