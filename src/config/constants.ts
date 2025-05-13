f330b7d779753bc22de33a766c89c817fe8fdb90
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-05-13 09:56:07 -0500

    chore: make graphql path configurable

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,9 @@
 export const EOL_REPORT_URL = '';
 export const GRAPHQL_HOST = 'https://api.nes.herodevs.com';
+export const GRAPHQL_PATH = '/graphql'
 
 export const config = {
   eolReportUrl: process.env.EOL_REPORT_URL || EOL_REPORT_URL,
   graphqlHost: process.env.GRAPHQL_HOST || GRAPHQL_HOST,
-  graphqlPath: '/graphql',
+  graphqlPath: process.env.GRAPHQL_PATH || GRAPHQL_PATH,
 };
