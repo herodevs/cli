83027ad0c457041590d4fab680469d3c00e87466
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-05-13 09:59:39 -0500

    chore: run linting

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const EOL_REPORT_URL = '';
 export const GRAPHQL_HOST = 'https://api.nes.herodevs.com';
-export const GRAPHQL_PATH = '/graphql'
+export const GRAPHQL_PATH = '/graphql';
 
 export const config = {
   eolReportUrl: process.env.EOL_REPORT_URL || EOL_REPORT_URL,
