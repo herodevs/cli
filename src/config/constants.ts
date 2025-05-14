a90de1aa2ae25443b383877b55c890a077a45329
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-05-14 09:44:42 -0500

    feat: add url for prod eol report card

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const EOL_REPORT_URL = '';
+export const EOL_REPORT_URL = 'https://eol-report-card.apps.herodevs.com';
 export const GRAPHQL_HOST = 'https://api.nes.herodevs.com';
 export const GRAPHQL_PATH = '/graphql';
 
