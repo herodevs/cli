04ce7e34f60ee3a790817bc2a287cb3b86db9d35
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2025-05-21 15:44:19 -0500

    chore: flip show vulns config

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,5 +6,5 @@ export const config = {
   eolReportUrl: process.env.EOL_REPORT_URL || EOL_REPORT_URL,
   graphqlHost: process.env.GRAPHQL_HOST || GRAPHQL_HOST,
   graphqlPath: process.env.GRAPHQL_PATH || GRAPHQL_PATH,
-  showVulnCount: false,
+  showVulnCount: true,
 };
