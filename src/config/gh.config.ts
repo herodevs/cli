30c7be88071f23cffd74147feb5037ba1d0314c6
Eduardo Roth <eduardo@eduardoroth.dev>
2025-12-09 17:29:07 -0600

    feat(gh): add gh list command

--- src/config/gh.config.ts
+++ src/config/gh.config.ts
@@ -4,11 +4,17 @@ const DEFAULT_GH_CLIENT_ID = 'Iv23liIePcrOHZ5tdjx7';
 const DEFAULT_GH_SERVICE_NAME = '@herodevs/cli';
 const DEFAULT_GH_ACCESS_KEY = 'gh_access_token';
 const DEFAULT_GH_REFRESH_KEY = 'gh_refresh_token';
+const DEFAULT_GH_API_BASE_URL = 'https://api.github.com/';
+const DEFAULT_GH_API_VERSION = '2022-11-28';
+const DEFAULT_GH_REPOS_PER_PAGE = 5;
 
 export const GH_CLIENT_ID = process.env.GH_CLIENT_ID ?? DEFAULT_GH_CLIENT_ID;
 export const GH_SERVICE_NAME = process.env.GH_SERVICE_NAME ?? DEFAULT_GH_SERVICE_NAME;
 export const GH_ACCESS_KEY = process.env.GH_ACCESS_KEY ?? DEFAULT_GH_ACCESS_KEY;
 export const GH_REFRESH_KEY = process.env.GH_REFRESH_KEY ?? DEFAULT_GH_REFRESH_KEY;
+export const GH_API_BASE_URL = process.env.GH_API_BASE_URL ?? DEFAULT_GH_API_BASE_URL;
+export const GH_API_VERSION = process.env.GH_API_VERSION ?? DEFAULT_GH_API_VERSION;
+export const GH_REPOS_PER_PAGE = Number(process.env.GH_REPOS_PER_PAGE ?? DEFAULT_GH_REPOS_PER_PAGE);
 
 export const GH_APP_OAUTH_SETTINGS: Pick<GitHubAppStrategyOptions, 'clientType' | 'clientId'> = {
   clientType: 'github-app',
--- src/service/gh.svc.ts
+++ src/service/gh.svc.ts
@@ -1,9 +1,43 @@
 import { Entry } from '@napi-rs/keyring';
 import { createOAuthDeviceAuth, type GitHubAppStrategyOptions } from '@octokit/auth-oauth-device';
-import { GH_ACCESS_KEY, GH_APP_OAUTH_SETTINGS, GH_REFRESH_KEY, GH_SERVICE_NAME } from '../config/gh.config.js';
+import {
+  GH_ACCESS_KEY,
+  GH_API_BASE_URL,
+  GH_API_VERSION,
+  GH_APP_OAUTH_SETTINGS,
+  GH_REFRESH_KEY,
+  GH_REPOS_PER_PAGE,
+  GH_SERVICE_NAME,
+} from '../config/gh.config.js';
+import type { Repo } from '../types/gh/repo.js';
 
 const accessTokenEntry = new Entry(GH_SERVICE_NAME, GH_ACCESS_KEY);
 const refreshTokenEntry = new Entry(GH_SERVICE_NAME, GH_REFRESH_KEY);
+
+const ghFetch = async <T>(endpoint: string, method: 'GET' | 'POST' | 'PUT' | 'DELETE', body?: unknown) => {
+  const response = await fetch(`${GH_API_BASE_URL}${endpoint}`, {
+    method,
+    headers: {
+      Accept: 'application/vnd.github+json',
+      ...(accessTokenEntry.getPassword() ? { Authorization: `Bearer ${accessTokenEntry.getPassword()}` } : {}),
+      'X-GitHub-Api-Version': GH_API_VERSION,
+    },
+    ...(body ? { body: JSON.stringify(body) } : {}),
+  });
+
+  if (!response.ok) {
+    switch (response.status) {
+      case 401:
+        throw new Error(
+          `Unauthorized access to ${GH_API_BASE_URL}${endpoint}. Please authorize the CLI running the gh authorize command`,
+        );
+      case 403:
+        throw new Error(`Forbidden access to ${GH_API_BASE_URL}${endpoint}`);
+    }
+    throw new Error(`An error occurred while fetching [${endpoint}]`);
+  }
+  return (await response.json()) as unknown as T;
+};
 
 export const userAccessToken = () => accessTokenEntry.getPassword();
 export const userRefreshToken = () => refreshTokenEntry.getPassword();
@@ -30,4 +64,8 @@ export const authenticateWithDeviceFlow = async (onVerification: GitHubAppStrate
   } catch (_err) {
     throw new Error(`An error occurred while trying to authorize with GitHub`);
   }
+};
+
+export const getUserRepositories = async (page: number = 1, per_page: number = GH_REPOS_PER_PAGE) => {
+  return await ghFetch<Repo[]>(`user/repos?per_page=${per_page}&page=${page}`, 'GET');
 };
