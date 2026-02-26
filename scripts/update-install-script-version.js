af5d4e3f1f9979e83c43de7f4cc9e496210a5388
Kevin Longmuir <kevin@herodevs.com>
2026-02-26 18:57:33 -0500

    feat: update install script to sync the Docker file image to the package version on release

--- scripts/update-install-script-version.js
+++ scripts/update-install-script-version.js
@@ -11,9 +11,11 @@ const __dirname = path.dirname(__filename);
 
 const readmePath = path.join(__dirname, '../README.md');
 const installScriptPath = path.join(__dirname, './install.sh');
+const dockerfilePath = path.join(__dirname, '../ci/image.Dockerfile');
 
 let readme = fs.readFileSync(readmePath, 'utf8');
 let installScript = fs.readFileSync(installScriptPath, 'utf8');
+let dockerfile = fs.readFileSync(dockerfilePath, 'utf8');
 
 // Regex to match version string inside /cli/v{{version}}/scripts/
 const versionInInstallReadme = /(\/cli\/)v\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(\/scripts\/)/g;
@@ -21,10 +23,16 @@ const versionInInstallReadme = /(\/cli\/)v\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(\/sc
 // Regex to match the LATEST_VERSION defined in the install.sh script
 const versionInInstallScript = /(LATEST_VERSION=")v\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(")/g;
 
+// Regex to match ARG VERSION=<version> in the Dockerfile
+const versionInDockerfile = /(ARG VERSION=)\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?/g;
+
 readme = readme.replace(versionInInstallReadme, `$1v${newVersion}$2`);
 fs.writeFileSync(readmePath, readme);
 
 installScript = installScript.replace(versionInInstallScript, `$1v${newVersion}$2`);
 fs.writeFileSync(installScriptPath, installScript);
+
+dockerfile = dockerfile.replace(versionInDockerfile, `$1${newVersion}`);
+fs.writeFileSync(dockerfilePath, dockerfile);
 
 console.log(`Updated install script and install command with ${newVersion}`);
