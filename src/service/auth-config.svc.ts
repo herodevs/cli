3dc42fead46492d2c366b421657a79e037de534f
Suneel shastry <suneel@herodevs.com>
2026-02-27 11:58:25 -0500

    fix: revert default client Id

--- src/service/auth-config.svc.ts
+++ src/service/auth-config.svc.ts
@@ -1,5 +1,5 @@
 const DEFAULT_REALM_URL = 'https://idp.prod.apps.herodevs.io/realms/universe/protocol/openid-connect';
-const DEFAULT_CLIENT_ID = 'default-public';
+const DEFAULT_CLIENT_ID = 'eol-ds';
 
 export function getRealmUrl() {
   return process.env.OAUTH_CONNECT_URL || DEFAULT_REALM_URL;
