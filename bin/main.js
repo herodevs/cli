e675e432f8c55b303072fc6da2ddab3eaae7969e
Kevin Longmuir <kevin@herodevs.com>
2026-03-05 16:30:20 -0500

    fix: version flag when running with no args

--- bin/main.js
+++ bin/main.js
@@ -2,13 +2,16 @@ import { parseArgs } from 'node:util';
 import { execute } from '@oclif/core';
 
 async function main(isProduction = false) {
-  const { positionals } = parseArgs({
+  const { positionals, values } = parseArgs({
     allowPositionals: true,
     strict: false, // Don't validate flags
+    options: {
+      version: { type: 'boolean', short: 'v' },
+    },
   });
 
-  // If no arguments at all, default to help
-  if (positionals.length === 0) {
+  // If no arguments at all, default to help (but not if --version/-v was passed)
+  if (positionals.length === 0 && !values.version) {
     process.argv.splice(2, 0, 'help');
   }
 
