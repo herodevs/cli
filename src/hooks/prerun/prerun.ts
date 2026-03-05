d0a5d3a9d6c411480287751f69b8c46e30f725a3
Kevin Longmuir <kevin@herodevs.com>
2026-03-05 17:11:36 -0500

    feat: detect and prevent hd update command in npm installation (#514)

--- src/hooks/prerun/prerun.ts
+++ src/hooks/prerun/prerun.ts
@@ -1,10 +1,24 @@
+import * as path from 'node:path';
 import type { Hook } from '@oclif/core';
 import debug from 'debug';
 
-const hook: Hook<'prerun'> = async (opts) => {
-  // If JSON flag is enabled, silence debug logging
-  if (opts.Command.prototype.jsonEnabled()) {
+const hook: Hook<'prerun'> = async function (this: Hook.Context, { Command }) {
+  if (Command.prototype.jsonEnabled()) {
     debug.disable();
+  }
+
+  if (Command.id === 'update') {
+    const isNpm = this.config.root.split(path.sep).includes('node_modules');
+
+    if (isNpm) {
+      this.warn('The update command is not supported for npm installations.');
+      this.log('\nTo update to the latest, run:\n');
+      this.log('  npm install -g @herodevs/cli@latest\n');
+      this.log('\nTo update to a specific version, run:\n');
+      this.log('  npm install -g @herodevs/cli@<version>\n');
+
+      process.exit(0);
+    }
   }
 };
 
