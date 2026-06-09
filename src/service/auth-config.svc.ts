7c11d4a895e1b01e9b6e4c735c576f05d1d1499d
Venancio Orozco <4390221+v3nant@users.noreply.github.com>
2026-06-09 09:20:13 -0600

    chore: replaces idp realm url

--- src/service/auth-config.svc.ts
+++ src/service/auth-config.svc.ts
@@ -1,14 +1,8 @@
-const DEFAULT_KC_BASE = 'https://auth.herodevs.com/idp';
-const DEFAULT_KC_REALM = 'universe';
+const DEFAULT_REALM_URL = 'https://auth.herodevs.com/idp/realms/universe/protocol/openid-connect';
 const DEFAULT_CLIENT_ID = 'eol-ds';
 
 export function getRealmUrl() {
-  if (process.env.OAUTH_CONNECT_URL) {
-    return process.env.OAUTH_CONNECT_URL;
-  }
-  const base = process.env.NES_KC_BASE || DEFAULT_KC_BASE;
-  const realm = process.env.NES_KC_REALM || DEFAULT_KC_REALM;
-  return `${base}/realms/${realm}/protocol/openid-connect`;
+  return process.env.OAUTH_CONNECT_URL || DEFAULT_REALM_URL;
 }
 
 export function getClientId() {
